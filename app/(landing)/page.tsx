c9f1f0a00e4b1eefd3e6e38cbe421affa91a0f5d
Tushar Aggarwal <beingatushar@gmail.com>
2023-11-25 14:10:04 +0530

    clerk added

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,10 +1,24 @@
+import { Button } from '@/components/ui/button'
 import React from 'react'
-
+import Link from "next/link"
 type Props = {}
 
 const LandingPage = (props: Props) => {
     return (
-        <div>LandingPage (Unprotected) </div>
+        <>
+
+            <div>LandingPage (Unprotected) </div>
+            <div>
+                <Link href="sign-in"  >
+                    <Button>Sign In</Button>
+                </Link>
+            </div>
+            <div>
+                <Link href="sign-up"  >
+                    <Button>Sign Up</Button>
+                </Link>
+            </div>
+        </>
     )
 }
 
