5e682aeff9110828e9d77c55af50742c22823b97
Tushar Aggarwal <beingatushar@gmail.com>
2023-11-25 17:13:50 +0530

    added conversation

--- components/mobile-sidebar.tsx
+++ components/mobile-sidebar.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import { useEffect, useState } from "react";
 import { Menu } from "lucide-react";
 
 import { Button } from "@/components/ui/button";
@@ -8,23 +7,17 @@ import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
 import Sidebar from "@/components/sidebar";
 
 export const MobileSidebar = () => {
-    const [isMounted, setIsMounted] = useState(false);
 
-    useEffect(() => {
-        setIsMounted(true);
-    }, []);
 
-    if (!isMounted) {
-        return null;
-    }
 
     return (
         <Sheet>
-            <SheetTrigger>
-                <Button variant="ghost" size="icon" className="md:hidden">
+            <Button asChild variant="ghost" size="icon" className="md:hidden">
+                <SheetTrigger>
                     <Menu />
-                </Button>
-            </SheetTrigger>
+                </SheetTrigger>
+            </Button>
+
             <SheetContent side="left" className="p-0">
                 <Sidebar />
             </SheetContent>
